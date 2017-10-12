9e1748bfd5d7630fa198bec8ddcd6322d7b09823
kojimizu <kojimizu@gmail.com>
2017-10-13 08:59:14 +0900

    Add files via upload

---
+++ ISLR.R
@@ -0,0 +1,23 @@
+
+# ISLR Review
+# Chap2: statistical learning
+
+#basic command
+x=c(1,6,2)
+y=c(1,4,3)
+
+# length - check lenfth of variables
+length(x)
+length(y)
+x+y
+
+#ls():list all of variables / rm():delete any
+ls()
+rm(x,y)
+ls()
+
+#rm function - remove all objects at once
+rm(list=ls())
+
+
+
